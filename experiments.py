e5c01d3860b0460936098559f79fd965c6c85793
Riccardo Massidda <contact@rmassidda.it>
2020-11-05 14:15:10 +0100

    Experiments defined using lambdas

--- experiments.py
+++ experiments.py
@@ -4,7 +4,7 @@ from numerical import qr, modified_qr, q1, back_substitution
 import numpy as np
 import time
 
-def solver(y, method, params, i, log):
+def solver(y, method, params):
     # Initial values
     f, g, Q = lls_functions(X_hat, X, y)
     w       = np.random.rand(n)
@@ -23,11 +23,12 @@ def solver(y, method, params, i, log):
     end    = time.time()
 
     # Log results
-    log['duration'][i] = end - start
-    log['residual'][i] = f(w_c)
-    log['steps'][i]    = s
+    duration = end - start
+    residual = f(w_c)
+    steps    = s
+    return duration, residual, steps
 
-def direct_solver(y, qr_factorization, i, log):
+def direct_solver(y, qr_factorization):
     # Initial values
     f, _, _ = lls_functions(X_hat, X, y)
     s       = 1
@@ -41,11 +42,32 @@ def direct_solver(y, qr_factorization, i, log):
     end      = time.time()
 
     # Log results
-    log['duration'][i] = end - start
-    log['residual'][i] = f(w_c)
-    log['steps'][i]    = s
+    duration = end - start
+    residual = f(w_c)
+    steps    = s
+    return duration, residual, steps
 
-metrics = ['duration','residual','steps']
+def solve_numpy(y):
+    # Initial values
+    f, _, _ = lls_functions(X_hat, X, y)
+    s       = 1
+
+    # Solve the LLS problem
+    start = time.time()
+    w_c   = np.linalg.lstsq(X_hat, y, rcond=None)[0]
+    end   = time.time()
+
+    # Log results
+    duration = end - start
+    residual = f(w_c)
+    steps    = s
+    return duration, residual, steps
+
+def run_experiment(f, Y, method):
+    log = np.zeros((MAX_EXP, 3))
+    for i, y in enumerate(Y):
+        log[i, :] = f(y)
+    print(method, *["%.2f" % np.average(log[:,j]) for j in range(3)],sep='\t')
 
 if __name__ == '__main__':
     # Data loading
@@ -53,67 +75,67 @@ if __name__ == '__main__':
     m, n     = X_hat.shape
 
     # Number of experiments
-    MAX_EXP  = 4
+    MAX_EXP  = 2
+    Y = [np.random.rand(m) for _ in range(MAX_EXP)]
 
-    # Test: evaluate performances on a random vector
-    methods = ['Newton', 'BFGS', 'LBFGS', 'QR*']
-    log = {k1: {k2: np.zeros(MAX_EXP) for k2 in metrics} for k1 in methods}
-    for i in range(MAX_EXP):
-        y = np.random.rand(m)
-        solver(y, Newton, {}, i, log['Newton'])
-        solver(y, BFGS, {'H': np.eye(n)}, i, log['BFGS'])
-        solver(y, LBFGS, {}, i, log['LBFGS'])
-        direct_solver(y, lambda A: modified_qr(A, m-n+1), i, log['QR*'])
-    for k1 in methods:
-        print(k1, *["%.2f" % np.average(log[k1][k2]) for k2 in metrics],sep='\t')
+    exp = lambda y: solve_numpy(y)
+    run_experiment(exp, Y, 'LLS Numpy')
 
-    # Test: evaluate different initialization for LBFGS
+    exp = lambda y: solver(y, Newton, {})
+    run_experiment(exp, Y, 'Newton')
+
+    exp = lambda y: solver(y, BFGS, {'H': np.eye(n)})
+    run_experiment(exp, Y, 'BFGS')
+
+    exp = lambda y: solver(y, LBFGS, {})
+    run_experiment(exp, Y, 'LBFGS')
+
+    exp = lambda y: direct_solver(y, lambda A: modified_qr(A, m-n+1))
+    run_experiment(exp, Y, 'QR*')
+
+    # Evaluate different memory for LBFGS
+    rng      = range(1, n, int(n/10))
+    methods  = ['t'+str(i) for i in rng]
+    params   = [{'t': i} for i in rng]
+    for method, p in zip(methods,params):
+        exp = lambda y: solver(y, LBFGS, p)
+        run_experiment(exp, Y, 'LBFGS '+method)
+
+    # Evaluate different initialization for LBFGS
     rng      = range(-5, 5, 2)
     methods  = ['γ', *['γ~1e'+str(i) for i in rng]]
     methods += ['I', *['I~1e'+str(i) for i in rng]]
     params   = [{'init': 'gamma'}, *[{'init': 'gamma', 'perturbate': 10**i} for i in rng]]
     params  += [{'init': 'identity'}, *[{'init': 'identity', 'perturbate': 10**i} for i in rng]]
-    log = {k1: {k2: np.zeros(MAX_EXP) for k2 in metrics} for k1 in methods}
-    for i in range(MAX_EXP):
-        y = np.random.rand(m)
-        for k1, p in zip(methods,params):
-            solver(y, LBFGS, p, i, log[k1])
-    for k1 in methods:
-        print(k1, *["%.2f" % np.average(log[k1][k2]) for k2 in metrics],sep='\t')
+    for method, p in zip(methods,params):
+        exp = lambda y: solver(y, LBFGS, p)
+        run_experiment(exp, Y, 'LBFGS '+method)
 
-    # Test: evaluate different initialization for BFGS
-    #       the matrix may be initialized using the identity
-    #       or the actual inverse of the Hessian. When this
-    #       is used, then the behaviour is the same as the
-    #       Newton method. Hereby both approaches are tested
-    #       with various perturbations.
+    # Evaluate different initialization for BFGS
     rng      = range(-5, 5, 2)
     methods  = ['H', *['H~1e'+str(i) for i in rng]]
     methods += ['I', *['I~1e'+str(i) for i in rng]]
     params   = [('H',0), *[('H',10**i) for i in rng]]
     params  += [('I',0), *[('I',10**i) for i in rng]]
-    log = {k1: {k2: np.zeros(MAX_EXP) for k2 in metrics} for k1 in methods}
-    for i in range(MAX_EXP):
-        y = np.random.rand(m)
+    def perturbate_H(y, eps, init):
         _, _, Q = lls_functions(X_hat, X, y)
         H = np.linalg.inv(Q)
-        for k1, (init, eps) in zip(methods,params):
-            if init == 'H':
-                e = {'H': H + np.random.normal(0,eps,n)}
-            else:
-                e = {'H': np.eye(n) + np.random.normal(0,eps,n)}
-            solver(y, BFGS, e, i, log[k1])
-    for k1 in methods:
-        print(k1, *["%.2f" % np.average(log[k1][k2]) for k2 in metrics],sep='\t')
+        if init == 'H':
+            return {'H': H + np.random.normal(0,eps,n)}
+        else:
+            return {'H': np.eye(n) + np.random.normal(0,eps,n)}
+    for method, (init, eps) in zip(methods,params):
+        exp = lambda y: solver(y, BFGS, perturbate_H(y, eps, init))
+        run_experiment(exp, Y, 'BFGS '+method)
 
-    # Test: evaluate different θ
-    n_int    = 4
-    methods  = ['π/'+"%.2f"%(2*n_int/i) if i != 0 else '0' for i in range(0,n_int+1)]
-    params   = [i*np.pi/(2*n_int) for i in range(0,n_int+1)]
-    log = {k1: {k2: np.zeros(MAX_EXP) for k2 in metrics} for k1 in methods}
-    for k1, theta in zip(methods,params):
-        _, y = theta_angled(X_hat, theta)
-        for i in range(MAX_EXP):
-            solver(y, LBFGS, {}, i, log[k1])
-    for k1 in methods:
-        print(k1, *["%.2f" % np.average(log[k1][k2]) for k2 in metrics],sep='\t')
+#     # Test: evaluate different θ
+#     n_int    = 4
+#     methods  = ['π/'+"%.2f"%(2*n_int/i) if i != 0 else '0' for i in range(0,n_int+1)]
+#     params   = [i*np.pi/(2*n_int) for i in range(0,n_int+1)]
+#     log = {k1: {k2: np.zeros(MAX_EXP) for k2 in metrics} for k1 in methods}
+#     for k1, theta in zip(methods,params):
+#         _, y = theta_angled(X_hat, theta)
+#         for i in range(MAX_EXP):
+#             solver(y, LBFGS, {}, i, log[k1])
+#     for k1 in methods:
+#         print(k1, *["%.2f" % np.average(log[k1][k2]) for k2 in metrics],sep='\t')
