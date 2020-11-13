d45abad43edfb5a63c173dfbf0dfbbe0a1e98c83
Riccardo Massidda <contact@rmassidda.it>
2020-11-13 10:06:59 +0100

    plot.py produces also the LaTeX tables required, other than all the required plots

--- plot.py
+++ plot.py
@@ -1,93 +1,146 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-# init
+# Load results of the experiments from file
+mod_results = np.load('results/mod_results.npy')
+std_results = np.load('results/std_results.npy')
+resid       = np.load('results/initializers.npy')
+theta_def   = np.load('results/theta_defaults.npy')
+X_cond      = np.load('results/theta_Xcond.npy')
+y_cond      = np.load('results/theta_ycond.npy')
+t_rng       = np.load('results/t_rng.npy')
+t_lbfgs     = np.load('results/t_lbfgs.npy')
+
+#
+# Conditioning of the problem for various \theta
+#
 fig = plt.figure()
-# fig.suptitle('LBFGS convergence')
 plt.yscale('log')
-plt.xlabel('Steps')
-# lbfgs convergence
-lbfgs_conv = np.load('results/lbfgs-convergence.npy')
-x = range(len(lbfgs_conv))
-plt.plot(x, lbfgs_conv, label=r'LBFGS $\gamma$')
-# lbfgs convergence
-lbfgs_conv = np.load('results/lbfgs-convergence-identity.npy')
-x = range(len(lbfgs_conv))
-plt.plot(x, lbfgs_conv, label=r'LBFGS $I$')
-# lbfgs convergence
-lbfgs_conv = np.load('results/bfgs-convergence.npy')
-x = range(len(lbfgs_conv))
-plt.plot(x, lbfgs_conv, label=r'BFGS $I$')
-# show
-plt.legend()
+plt.xlabel(r'$\theta$')
+plt.plot(range(X_cond.shape[0]), X_cond, label=r'$\kappa_{rel,\hat{X}\to w}$')
+plt.plot(range(y_cond.shape[0]), y_cond, label=r'$\kappa_{rel,y \to w}$')
+plt.legend(fontsize="x-large")
 plt.show()
 
-# solver, granularity, (t, r, s)
-theta_defaults = np.load('results/theta_defaults.npy')
+#
+# Average residual, time and steps for different methods and various \theta
+#
 def_names      = ['LLS Numpy', 'Newton', 'LBFGS', 'QR*']
-titles         = ['Time', 'Residual', 'Steps']
-x = np.linspace(0, np.pi/2, theta_defaults.shape[1])
+titles         = ['Time (s)', 'Residual', 'Steps']
+x = np.linspace(0, np.pi/2, theta_def.shape[1])
 for i, title in enumerate(titles):
     fig = plt.figure()
     plt.xlabel(r'$\theta$')
-    # fig.suptitle(title)
+    plt.ylabel(title)
     if title == 'Residual' or title == 'Steps':
         plt.yscale('log')
     for j, name in enumerate(def_names):
         if ('QR' in name or 'LLS' in name) and title=='Steps':
             continue
-        plt.plot(x, theta_defaults[j, :, i], label=name)
+        if 'Newton' in name:
+            continue
+        plt.plot(x, theta_def[j, :, i], label=name)
     plt.legend()
     plt.show()
 
-# Narrow
-def_names      = ['LLS Numpy', 'Newton', 'LBFGS', 'QR*']
-titles         = ['Time', 'Residual', 'Steps']
-x = np.linspace(0, np.pi/2, theta_defaults.shape[1])
+#
+# Average residual, time and steps for different methods and various \theta
+# in a narrow interval
+#
+x = np.linspace(0, np.pi/2, theta_def.shape[1])
 a = int(len(x) / 4)
 b = a * 3
 for i, title in enumerate(titles):
     fig = plt.figure()
     plt.xlabel(r'$\theta$')
-    # fig.suptitle(title)
-    if title == 'Residual' or title == 'Steps':
+    plt.ylabel(title)
+    if title == 'Residual':
         plt.yscale('log')
     for j, name in enumerate(def_names):
         if ('QR' in name or 'LLS' in name) and title=='Steps':
             continue
-        plt.plot(x[a:b], theta_defaults[j, a:b, i], label=name)
+        plt.plot(x[a:b], theta_def[j, a:b, i], label=name)
     plt.legend()
     plt.show()
 
-# conditioning
-X_cond = np.load('results/theta_Xcond.npy')
-y_cond = np.load('results/theta_ycond.npy')
-fig = plt.figure()
-# fig.suptitle('Conditioning')
-plt.yscale('log')
-plt.xlabel(r'$\theta$')
-plt.plot(x, X_cond, label=r'$\kappa_{rel,\hat{X}\to w}$')
-plt.plot(x, y_cond, label=r'$\kappa_{rel,y \to w}$')
-plt.legend(fontsize="x-large")
-plt.show()
+#
+# Graphical depiction of the r-convergence of the LBFGS method
+# convergence (linscale e logscale)
+#
+limit = np.argwhere(resid[0] == -1)[0,0]
+resid = resid[0, :limit]
+f_opt = resid[-1]
+diff  = resid - f_opt
+r_rng = np.linspace(0, 1, 100)
+for r in r_rng:
+    comp = [diff[0]*(r**i) for i in range(limit)]
+    if (diff <= comp).all():
+        break
+print('r-line', r)
+scale = ['linear', 'log']
+for s in scale:
+    fig = plt.figure()
+    plt.xlabel('Steps')
+    plt.yscale(s)
+    plt.plot(range(limit), diff, label=r'$f(w_i) - f(w^*)$')
+    plt.plot(range(limit), comp, label=r'$r^i(f(w_0) - f(w^*))$')
+    plt.legend()
+    plt.show()
 
-# lbfgs memory
-t_lbfgs = np.load('results/t_lbfgs.npy')
-t_rng   = np.load('results/t_rng.npy')
-titles  = ['time', 'residual', 'steps']
+#
+# Plot of the residual curve for the LBFGS method (linscale e logscale)
+#
+fig = plt.figure()
+scale = ['linear', 'log']
+for s in scale:
+    fig = plt.figure()
+    plt.xlabel('Steps')
+    plt.yscale(s)
+    plt.plot(range(limit), resid)
+    plt.show()
+
+#
+# Average residual, time and steps for different memory values in LBFGS
+#
+titles  = ['Time (s)', 'residual', 'steps']
 for i, title in enumerate(titles):
     fig = plt.figure()
     plt.xlabel(r'$t$')
     plt.ylabel(title)
-    if title == 'Residual':
-        plt.yscale('log')
     plt.plot(t_rng, t_lbfgs[:, i])
     plt.show()
 
-# init, perturbation steps
-init_lbfgs = np.load('results/init_lbfgs.npy')
 try:
     from tabulate import tabulate
-    print(tabulate(init_lbfgs, tablefmt='latex'))
+    no_tab = False
 except ModuleNotFoundError:
-    print(init_lbfgs)
+    no_tab = True
+
+#
+# Table avg for the intervals in the narrow
+#
+x = np.linspace(0, np.pi/2, theta_def.shape[1])
+a = int(len(x) / 4)
+b = a * 3
+models = np.array(['LLS Numpy', 'Newton', 'LBFGS', 'QR*']).reshape((4,1))
+heads  = ['Model', 'Time', 'Residual', 'Steps']
+avg_metrics = np.average(theta_def[:, a:b, :], axis=1)
+avg_metrics = np.concatenate((models, avg_metrics), axis=1)
+if no_tab:
+    print(heads)
+    print(avg_metrics)
+else:
+    print(tabulate(avg_metrics, headers=heads, tablefmt='latex'), end='\n\n')
+
+#
+# Comparison of QR and QR*
+#
+mod_results = np.concatenate((['QR*'], np.average(mod_results, axis=0))).reshape((1,4))
+std_results = np.concatenate((['QR'], np.average(std_results, axis=0))).reshape((1,4))
+qr_compare  = np.concatenate((mod_results, std_results), axis=0)
+heads  = ['Model', 'Time', 'Residual', 'Steps']
+if no_tab:
+    print(heads)
+    print(qr_compare)
+else:
+    print(tabulate(qr_compare, headers=heads, tablefmt='latex'), end='\n\n')
